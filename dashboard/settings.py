e0edb7d8096cd0f093ac3fadb2f0a14aa001a07e
Tung Duong <duongthanhtungvn01@gmail.com>
2020-05-15 21:39:45 +0700

    Update

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
     #'default': {
-    #   'ENGINE': 'django.db.backends.mysql',
-    #   'NAME': 'dashboard',
-    #   'USER': 'dashboard',
-    #   'PASSWORD': 'abc@123',
-    #   'HOST': 'localhost',
-    #   'PORT': '',
-   #}
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+
+    'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'dashboard',
+       'USER': 'dashboard',
+       'PASSWORD': 'abc@123',
+       'HOST': 'localhost',
+       'PORT': '',
+    }
 }
 
 
