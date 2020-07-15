0221278990f0359e2039efbe35f57b16863381b6
Tung Duong <duongthanhtungvn01@gmail.com>
2020-07-15 08:16:31 +0100

    Update

--- app/views_api.py
+++ app/views_api.py
@@ -440,7 +440,7 @@ def userCheckIn(request):
 
     return Response({
         'success': status == CheckIn.Status.SUCCESS, 
-        'statusCode': 1 if status == CheckIn.Status.SUCCESS else (2 if status != SCAN_NOT_TIME_OUT_YET else 3)
+        'statusCode': 1 if status == CheckIn.Status.SUCCESS else (2 if status != SCAN_NOT_TIME_OUT_YET else 3),
         'message': CheckIn.Status.mobile_messages.get(status, '') % tuple(message_params)
     })
 
