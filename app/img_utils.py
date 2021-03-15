d6f0dbcbee66dcd30a3c01bd583ffea93387d1a7
tung.duong <duongthanhtungvn01@gmail.com>
2021-03-15 16:02:17 +0700

    Update

--- app/img_utils.py
+++ app/img_utils.py
@@ -12,8 +12,9 @@ def resizeProfileImage(imgField):
     sz = min(w, h, settings.PROFILE_IMAGE_SIZE)
 
     image_data = np.array(image)
-    image_data = image_data[:,:,:3]
-
+    if len(image_data.shape) == 3:
+        image_data = image_data[:,:,:3]
+        
     if h > w:
         image_data = image_data[:w]
     
