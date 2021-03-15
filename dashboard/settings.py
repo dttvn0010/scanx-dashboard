da05e5368d296339a4bd82bf3a98d9c8616897b2
tung.duong <duongthanhtungvn01@gmail.com>
2021-03-15 15:08:32 +0700

    Update

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
     'default': {
        'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'dashboard',
+       'NAME': 'dashboard_dev',
        'USER': 'dashboard',
        'PASSWORD': 'abc@123',
        'HOST': 'localhost',
